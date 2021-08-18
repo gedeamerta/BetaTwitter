440cbb1b95d5f7d36da7a3d144312691c0b4aa9e
gdamerta <amerta213bali@gmail.com>
2021-08-18 15:10:11 +0800

    failure doing post unit testing
    i can't get it done, sorry

--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -1,5 +1,51 @@
+require '../../db/mysql_connector.rb'
 require_relative '../../controllers/post_controller.rb'
+require_relative '../../models/post.rb'
+require_relative '../../models/post_detail.rb'
 
 describe PostController do
-    
+
+    # describe '#show_posts' do
+    #     context 'when data is exist' do
+    #         it 'should show all data posts' do
+    #             controller = PostController.new
+    #             response = controller.show_all_post
+    #             expected_view = ERB.new(File.read('../../views/posts.erb')).result(binding)
+    #             expect(expected_view).to eq(response)
+    #         end
+    #     end
+    # end
+
+    # describe '#save_post' do
+    #     before(:each) do
+    #         client = create_db_client
+    #         client.query("TRUNCATE posts")
+    #     end
+    #     context 'when given valid parameter' do
+    #         before(:each) do
+    #             params = {
+    #                 'caption' => 'caption nih boss',
+    #                 'image' => '/img/testing',
+    #                 'id_user' => 3,
+    #                 'post_hashtag' => '#check1'.match(/[^#]\S+/)
+    #             }
+
+    #             controller = PostController.new
+
+    #             response = controller.create_post(params)
+    #         end
+
+    #         it 'should save order' do
+    #             expected_post = Post.get_posts_by_id('posts.id_post')
+    #             expect(expected_post).not_to be_nil
+    #         end
+
+    #         it 'should render page' do
+    #             posts = Post.get_all_post
+    #             post_tags = Post_Detail.get_all_tag_trending
+    #             expected_view = ERB.new(File.read('../../views/posts.erb')).result(binding)
+    #             expect(expected_view).to eq(@response)
+    #         end
+    #     end
+    # end
 end
