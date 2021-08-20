a6150399b98c638cacdd0c88f8cca91e85390002
gdamerta <amerta213bali@gmail.com>
2021-08-20 16:58:31 +0800

    update readme

--- db/mysql_connector.rb
+++ db/mysql_connector.rb
@@ -2,8 +2,8 @@ require 'mysql2'
 def create_db_client 
     client = Mysql2::Client.new(
         :host => "localhost",
-        :username => "amerta",
-        :password => "Amerta123#@!",
+        :username => "root",
+        :password => "",
         :database => "beta_twitter"
     )
     client
